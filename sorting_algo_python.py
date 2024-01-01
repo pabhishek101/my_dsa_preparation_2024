e945e1f39c5c441d9b05ab1106a09d478be76954
Abhishek Patel <pabhi199@gmail.com>
2024-01-01 14:24:39 +0530

    corrected selection sort code

--- sorting_algo_python.py
+++ sorting_algo_python.py
@@ -79,7 +79,7 @@ def Selection_sort(arr:list)->list:
     n=len(arr)
     for i in range(n-1):
         min=i
-        for j in range(1,n):
+        for j in range(i+1,n):
             if arr[j]<arr[min]:
                 min=j
         
