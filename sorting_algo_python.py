d413675b347f15c2da09eea8bfcaff75d655871a
Abhishek Patel <pabhi199@gmail.com>
2024-01-01 14:05:35 +0530

    selection sort and Merge sort done.

---
+++ sorting_algo_python.py
@@ -0,0 +1,113 @@
+#--------------sorting algo in python---------------#
+
+# 1. Bubble sort
+# 2. Selection sort
+# 3. Insertion sort
+# 4. Merge sort
+# 5. Quick sort
+# 6. Heap sort
+# 7. Shell sort
+# 8. Tree sort
+# 9. Counting sort
+# 10. Bucket sort
+# 11. Radix sort
+# 12. Topological sort
+
+
+# 1 Bubble sort
+
+#-------------------------------Key Analysis-----------------------------#
+
+# time complexity:
+#               (Worst case): O(n**2)        -- list is sorted in descending order.
+#               (Best case) : O(n)           -- list is already sorted.
+#               (Avg case)  : O(n**2)
+
+# space complexity:
+#               (worst case): O(1)
+
+# Stable Algorithms
+# Adaptable (after applying Flag)
+
+# Application:
+#           1. In Nth pass we get Nth largest element in sorted position.
+#           2. 
+
+#-------------------------------Bubble Sort Code Start-----------------------------#
+
+def Bubble_sort(arr:list)->list:
+    n=len(arr)
+    for _ in range(n-1):
+        swap=0
+        for j in range(n-1):
+            if arr[j]>arr[j+1]:
+                swap=1
+                temp=arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=temp
+        if swap==0:
+            return
+    return arr
+
+#-------------------------------Bubble Sort Code End-----------------------------#
+
+
+
+
+# 2 Selection sort
+
+#-------------------------------Key Analysis-----------------------------#
+
+# time complexity:
+#               (Worst case): O(n**2)        -- list is sorted in descending order.
+#               (Best case) : O(n)           -- list is already sorted.
+#               (Avg case)  : O(n**2)
+
+# space complexity:
+#               (worst case): O(1)
+
+# Stable Algorithms
+# Adaptable (after applying Flag)
+
+# Application:
+#           1. Inplace sort Algorithms(no extra space required).
+#           2. 
+
+#-------------------------------Selection Sort Code Start-----------------------------#
+
+def Selection_sort(arr:list)->list:
+    n=len(arr)
+    for i in range(n-1):
+        min=i
+        for j in range(1,n):
+            if arr[j]<arr[min]:
+                min=j
+        
+        # swapping
+        temp=arr[min]
+        arr[min]=arr[i]
+        arr[i]=temp
+    return arr
+
+#-------------------------------Selection Sort Code End-----------------------------#
+
+
+
+
+
+
+#------------main function-------------------#
+
+
+if __name__=='__main__':
+    arr=[5,9,7,4,3,8,1]
+    
+    print("Original array: ")
+    print(arr)
+    print()
+    print("array after applying bubble sort: ")
+    print(Bubble_sort(arr))
+    print()
+    print("array after applying Selection sort: ")
+    print(Selection_sort(arr))
+    print()
